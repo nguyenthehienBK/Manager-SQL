06cce7593335814b34a67c3f9cf43849af0c57d8
Nguyễn Thế Hiển - Falcon <hien.nt8@kiotviet.com>
2023-11-01 10:23:10 +0700

    sync

---
+++ Lakehouse/kv_product_suggestion.vw_barcode_clean.sql
@@ -0,0 +1,260 @@
+-- kv_product_suggestion.vw_barcode_clean source
+
+CREATE OR REPLACE VIEW kv_product_suggestion.vw_barcode_clean (
+  retailer_key,
+  product_key,
+  product_id,
+  product_name,
+  code,
+  barcode,
+  unit,
+  created_date,
+  category_key,
+  description,
+  valid_barcode,
+  barcode_clean_package,
+  package_size,
+  barcode_clean)
+TBLPROPERTIES (
+  'transient_lastDdlTime' = '1696844762')
+AS WITH
+  kv_barcode AS (
+SELECT
+    retailer_key AS retailer_key,
+    product.product_key AS product_key,
+    product.real_key AS product_id,
+    RTRIM( LTRIM( REGEXP_REPLACE( REPLACE( REPLACE(product.name, '\n', ' '), '\r', ' '), r'\[.*?\+-]', '') ) ) AS product_name,
+    product.code,
+    product.barcode,
+    CASE
+        WHEN product.barcode IS NULL THEN REGEXP_REPLACE(regexp_replace(product.code, '(\\{DEL}+)', ''), '(\\{DEL1}+)', '')
+        -- nếu 1 product có barcode thì sử dụng giá trị đó
+        ELSE
+    REGEXP_REPLACE(regexp_replace(product.barcode,
+        '(\\{DEL}+)',
+        ''), '(\\{DEL1}+)', '')
+    END
+    AS barcode_final,
+    LOWER(product.unit) AS unit,
+    product.created_date AS created_date,
+    product.category_key,
+    product.description
+FROM
+    kvretail_warehouse.product_dim product
+WHERE
+    1 = 1 ),
+  barcode_int AS (
+SELECT
+    retailer_key,
+    product_key,
+    product_id,
+    product_name,
+    code,
+    barcode,
+    REGEXP_REPLACE( REGEXP_REPLACE(barcode_final, r'\+', ''), '-', '') AS barcode_clean,
+    unit,
+    created_date,
+    category_key,
+    description
+FROM
+    kv_barcode
+WHERE
+    1 = 1
+    AND CAST(barcode_final AS INT) IS NOT NULL ),
+  barcode_char AS (
+SELECT
+    retailer_key,
+    product_key,
+    product_id,
+    product_name,
+    code,
+    barcode,
+    CASE
+        -- Neu chua - va phan tu dau tien truoc dau - la so INT thi split va lay phan tu do
+      WHEN REGEXP_LIKE(barcode_final,
+        '[-]')
+            AND CAST (SUBSTRING(SPLIT_PART(barcode_final, '-', 1),-1) AS int) IS NOT NULL THEN SUBSTRING(SPLIT_PART(barcode_final, '-', 1),-1)
+            -- Neu chua - va phan tu dau tien sau dau - la so INT thi split va lay phan tu do
+            WHEN REGEXP_LIKE(barcode_final,
+            '[-]')
+                AND CAST(SUBSTRING(SPLIT_PART(barcode_final,
+          '-',
+          2), 1, 1) AS INT) IS NOT NULL THEN SUBSTRING(SPLIT_PART(barcode_final,
+        '-',
+        2), 1, 1)
+                -- Neu chua _ va phan tu dau tien truoc dau _ la so INT thi split va lay phan tu do
+                WHEN REGEXP_LIKE(barcode_final,
+                '[_]')
+                    AND CAST(SUBSTRING(SPLIT_PART(barcode_final, '_', 1),-1) AS INT) IS NOT NULL THEN SUBSTRING(SPLIT_PART(barcode_final, '_', 1),-1)
+                    -- Neu chua _ va phan tu dau tien sau dau _ la so INT thi split va lay phan tu do
+                    WHEN REGEXP_LIKE(barcode_final,
+                    '[_]')
+                        AND CAST(SUBSTRING(SPLIT_PART(barcode_final,
+          '_',
+          2), 1, 1) AS INT) IS NOT NULL THEN SUBSTRING(SPLIT_PART(barcode_final,
+        '_',
+        2), 1, 1)
+                        -- Neu chua + va phan tu dau tien truoc dau + la so INT thi split va lay phan tu do
+                        WHEN REGEXP_LIKE(barcode_final,
+                        r'[\+]')
+                            AND CAST(SUBSTRING(SPLIT_PART(barcode_final, '+', 1),-1) AS INT) IS NOT NULL THEN SUBSTRING(SPLIT_PART(barcode_final, '+', 1),-1)
+                            -- Neu chua + va phan tu dau tien sau dau + la so INT thi split va lay phan tu do
+                            WHEN REGEXP_LIKE(barcode_final,
+                            r'[\+]')
+                                AND CAST(SUBSTRING(SPLIT_PART(barcode_final,
+          '+',
+          2), 1, 1) AS INT) IS NOT NULL THEN SUBSTRING(SPLIT_PART(barcode_final,
+        '+',
+        2), 1, 1)
+                                -- Neu chua / va phan tu dau tien truoc dau / la so INT thi split va lay phan tu do
+                                WHEN REGEXP_LIKE(barcode_final,
+                                '[/]')
+                                    AND CAST(SUBSTRING(SPLIT_PART(barcode_final, '/', 1),-1) AS INT) IS NOT NULL THEN SUBSTRING(SPLIT_PART(barcode_final, '/', 1),-1)
+                                    -- Neu chua / va phan tu dau tien sau dau / la so INT thi split va lay phan tu do
+                                    WHEN REGEXP_LIKE(barcode_final,
+                                    '[/]')
+                                        AND CAST(SUBSTRING(SPLIT_PART(barcode_final,
+          '/',
+          2), 1, 1) AS INT) IS NOT NULL THEN SUBSTRING(SPLIT_PART(barcode_final,
+        '/',
+        2), 1, 1)
+                                        -- Neu chua \ va phan tu dau tien truoc dau \ la so INT thi split va lay phan tu do
+                                        WHEN REGEXP_LIKE(barcode_final,
+                                        r'[\\]')
+                                            AND CAST(SUBSTRING(SPLIT_PART(barcode_final, '\\', 1),-1) as INT) is not null then substring(SPLIT_PART(barcode_final, '\\', 1),-1)
+                                            -- Neu chua \ va phan tu dau tien sau dau \ la so INT thi split va lay phan tu do
+                                            WHEN REGEXP_LIKE(barcode_final,
+                                            r'[\\]')
+                                                AND CAST(SUBSTRING(SPLIT_PART(barcode_final,
+                '\\', 2), 1, 1) as INT) is not null then substring(SPLIT_PART(barcode_final, '\\', 2), 1, 1)
+                                                -- Neu chua . va phan tu dau tien truoc dau . la so INT thi split va lay phan tu do
+                                                WHEN REGEXP_LIKE(barcode_final,
+                                                r'[.]')
+                                                    AND CAST(SUBSTRING(SPLIT_PART(barcode_final, '.', 1),-1) AS INT) IS NOT NULL THEN SUBSTRING(SPLIT_PART(barcode_final, '.', 1),-1)
+                                                    -- Neu chua . va phan tu dau tien sau dau . la so INT thi split va lay phan tu do
+                                                    WHEN REGEXP_LIKE(barcode_final,
+                                                    r'[.]')
+                                                        AND CAST(SUBSTRING(SPLIT_PART(barcode_final,
+                      '.',
+                      2), 1, 1) AS INT) IS NOT NULL THEN SUBSTRING(SPLIT_PART(barcode_final,
+                    '.',
+                    2), 1, 1)
+                                                        -- Neu chua { va phan tu dau tien truoc dau { la so INT thi split va lay phan tu do
+                                                        WHEN REGEXP_LIKE(barcode_final,
+                                                        r'[{]')
+                                                            AND CAST(SUBSTRING(SPLIT_PART(barcode_final, '{', 1),-1) AS INT) IS NOT NULL THEN SUBSTRING(SPLIT_PART(barcode_final, '{', 1),-1)
+                                                            -- Neu chua } va phan tu dau tien sau dau } la so INT thi split va lay phan tu do
+                                                            WHEN REGEXP_LIKE(barcode_final,
+                                                            r'[}]')
+                                                                AND CAST(SUBSTRING(SPLIT_PART(barcode_final,
+                      '}',
+                      2), 1, 1) AS INT) IS NOT NULL THEN SUBSTRING(SPLIT_PART(barcode_final,
+                    '}',
+                    2), 1, 1)
+                                                                -- Neu ki tu dau tien la chu cai thi xoa ki tu do
+                                                                WHEN REGEXP_LIKE(SUBSTRING(barcode_final, 1, 1),
+                                                                '[a-zA-Z]') THEN SUBSTRING(barcode_final, 2)
+                                                                -- Neu ki tu cuoi cung la chu cai thi xoa ki tu do
+                                                                WHEN REGEXP_LIKE(SUBSTRING(barcode_final,-1),
+                                                                '[a-zA-Z]')THEN SUBSTRING(barcode_final, 1, LENGTH(barcode_final)-1)
+                                                                ELSE
+                barcode_final
+                                                            END
+                AS barcode_clean,
+                                                            unit,
+                                                            created_date,
+                                                            category_key,
+                                                            description
+                                                        FROM
+                                                            kv_barcode
+                                                        WHERE
+                                                            1 = 1
+                                                            AND CAST(barcode_final AS INT) IS NULL ),
+combined AS (
+SELECT
+                *,
+                CASE
+                  WHEN ( (
+        SELECT
+            reflect('com.citigo.data.udf.udf_checkGS1',
+            'checkGS1',
+            barcode_char.barcode_clean) = TRUE)
+            AND CAST(barcode_clean AS INT) IS NOT NULL
+                AND length (barcode_clean) IN (8, 10, 12, 13, 14)
+                    AND NOT REGEXP_LIKE(LOWER(barcode_clean),
+                    r'(^[a-z]|^(00|111|777|1234)|\D|_|\\.)') ) THEN 1
+        ELSE
+                0
+    END
+                AS valid_barcode
+FROM
+                barcode_char
+UNION ALL
+SELECT
+                *,
+                CASE
+                  WHEN ( (
+        SELECT
+            reflect('com.citigo.data.udf.udf_checkGS1',
+            'checkGS1',
+            barcode_int.barcode_clean) = TRUE)
+            AND CAST(barcode_clean AS INT) IS NOT NULL
+                AND length (barcode_clean) IN (8, 10, 12, 13, 14)
+                    AND NOT REGEXP_LIKE(LOWER(barcode_clean),
+                    r'(^[a-z]|^(0|111|777|1234)|\D|_|\\.)') ) THEN 1
+        ELSE
+                0
+    END
+                AS valid_barcode
+FROM
+                barcode_int ),
+-- các sản phẩm 14 chữ số, nếu chữ số đầu tiên khác 8, thì là sản phẩm theo dạng multi-item
+combined_multiitem AS (
+SELECT
+                `(barcode_clean)?+.+`
+                ,
+                CASE
+                  WHEN LENGTH(barcode_clean) = 14
+            AND SUBSTR(barcode_clean, 1, 1) <> '8'
+                AND valid_barcode = 1 THEN (
+                SELECT
+                    (CONCAT(SUBSTR(barcode_clean, 2, LENGTH(barcode_clean)-2), reflect('com.citigo.data.udf.udf_calculate_GS1_check_digit', 'calculate_GS1_check_digit', SUBSTR(barcode_clean, 2, LENGTH(barcode_clean)-2) ) )))
+                ELSE
+                barcode_clean
+            END
+                AS barcode_clean
+                ,
+                CASE
+                  WHEN LENGTH(barcode_clean) = 14
+                    AND SUBSTR(barcode_clean, 1, 1) <> '8'
+                        AND valid_barcode = 1 THEN barcode_clean
+                    END
+                AS barcode_clean_package,
+                    CASE
+                        WHEN LENGTH(barcode_clean) = 14
+                            AND SUBSTR(barcode_clean, 1, 1) <> '8'
+                                AND valid_barcode = 1 THEN 'multi-item'
+                                ELSE
+                'single-item'
+                            END
+                AS package_size
+                        FROM
+                            combined ),
+combined_padded AS (
+SELECT
+                `(barcode_clean)?+.+`
+                ,
+                CASE
+                  WHEN LENGTH(barcode_clean) = 12 THEN CONCAT('0', barcode_clean)
+        ELSE
+                barcode_clean
+    END
+                AS barcode_clean
+FROM
+                combined_multiitem )
+            SELECT
+              *
+FROM
+              combined_padded
+WHERE
+              1 = 1;
---
+++ Lakehouse/vw_new_product.sql
@@ -0,0 +1,271 @@
+-- kv_product_suggestion.vw_new_product source
+
+CREATE OR REPLACE VIEW kv_product_suggestion.vw_new_product (
+  _id,
+  industry_origin,
+  content,
+  barcode,
+  description,
+  industry_name,
+  industry_new,
+  img,
+  with_barcode,
+  timestamp)
+TBLPROPERTIES (
+  'transient_lastDdlTime' = '1698745828')
+AS with active_1y as (
+select
+    product_key,
+    timestamp
+from
+    (
+    select
+        product_key
+    ,
+        timestamp
+    ,
+        ROW_NUMBER() OVER (PARTITION BY product_key
+    ORDER BY
+        timestamp DESC) rn
+    from
+        (
+        select
+            product_key
+      ,
+            date(timestamp) as timestamp
+        from
+            kvretail_warehouse.invoice_detail_fact
+        where
+            timestamp >= DATE_SUB(CURRENT_DATE(),
+            7)
+    union all
+        select
+            product_key
+      ,
+            date(created_date) as timestamp
+        from
+            kvretail_warehouse.product_dim
+        where
+            date(created_date) >= DATE_SUB(CURRENT_DATE(),
+            7)
+    union all
+        SELECT
+            product_key
+     		,
+            max(created_date) as timestamp
+        from
+            kvretail_warehouse.purchase_order_detail_fact
+            -- Lấy thêm sản phẩm được nhập từ supplier
+        where
+            created_date >= DATE_SUB(CURRENT_DATE(),
+            7)
+        group by
+            product_key)
+) as temp_union
+where
+    temp_union.rn = 1
+)
+,
+raw as (
+SELECT
+    kv_product.retailer_key
+,
+    kv_retailer.real_key as retailer_id
+,
+    kv_industry.v_industry_name as kv_industry
+,
+    kv_product.product_key
+,
+    kv_product.barcode_clean as barcode
+,
+    kv_product.product_name as product_name_original
+,
+    RTRIM(
+    LTRIM(
+      REGEXP_REPLACE(
+        REGEXP_REPLACE(
+                REPLACE(
+                        REPLACE(
+                          REPLACE(
+                            REGEXP_REPLACE(
+                              REGEXP_REPLACE(UPPER(kv_product.product_name), r'\(.*?\)', '')
+                            , r'\[.*?\]', '')
+                          , '\n', ' ')
+                        , '\r', ' ')
+                      , '  ', ' ')
+              , r'MẪU MỚI|HCN.|ĐỒNG GIÁ|KHUYẾN MÃI|KHUYẾN MẠI|TẶNG|KÈM|TẶNG KÈM|KHAI TRƯƠNG|GIÁ SỐC', '')-- Loại bỏ một số cụm từ đặc trưng của từng gian hàng
+      , r' - ', ' ')-- Thay gạch nối bằng khoảng trắng
+    )
+  ) AS product_name
+,
+    kv_industry.industry_key
+,
+    active_1y.timestamp
+,
+    kv_product.description as description
+FROM
+    kv_product_suggestion.barcode_clean kv_product
+JOIN active_1y on
+    active_1y.product_key = kv_product.product_key
+    -- được tạo hoặc có giao dịch trong vòng 1 năm trở lại
+JOIN kvretail_warehouse.retailer_dim as kv_retailer on
+    kv_retailer.retailer_key = kv_product.retailer_key
+JOIN kvretail_warehouse.industry_dim as kv_industry ON
+    kv_industry.industry_key = kv_retailer.industry_key
+where
+    1 = 1
+    and kv_product.valid_barcode = 1
+    and kv_industry.industry_key in (1, 2, 5, 6, 7, 9, 11, 12, 13, 15, 27)
+        
+),
+
+kv_img_url as (
+-- Lay image url cua cac san pham
+SELECT
+    product_key,
+    real_key,
+    image
+FROM
+    kvretail_warehouse.product_image_dim
+)
+,
+kv_barcode as
+(
+select
+    reflect('com.citigo.udf.from_bq.CleanKvProductName',
+    'cleanKvProductName',
+    raw.product_name) as name
+    -- Clean tên sản phẩm chứa cả giá ("K") và chuỗi ("SALE")
+,
+    raw.barcode as barcode
+,
+    img_url.image as images
+,
+    raw.description as description
+,
+    CASE
+        when industry_key = 27 then 9
+        else industry_key
+    END as industry
+,
+    'kiotviet' as barcode_source
+,
+    raw.kv_industry as industry_name
+,
+    raw.timestamp as timestamp
+from
+    raw
+join kv_img_url img_url on
+    raw.product_key = img_url.product_key
+where
+    length(img_url.image) > 25
+    and length(barcode) >= 8
+),
+
+kv_barcode_with_name_most_use as
+(
+SELECT
+    *
+from
+    (
+    SELECT
+        temp.barcode
+			,
+        temp.name
+			,
+        temp.cnt
+			,
+        ROW_NUMBER() OVER (PARTITION BY barcode
+    ORDER BY
+        temp.cnt DESC) rn
+    from
+        (
+        SELECT
+            barcode
+				,
+            name
+				,
+            count(*) as cnt
+        from
+            kv_barcode
+        group by
+            barcode,
+            name) as temp)
+where
+    rn = 1
+),
+kv_barcode_final as (
+SELECT
+    kb.name
+		,
+    kb.barcode 
+		,
+    kb.images
+		,
+    kb.description
+		,
+    kb.industry
+		,
+    kb.industry_name
+		,
+    kb.barcode_source
+		,
+    kb.timestamp
+		,
+    ROW_NUMBER () OVER (PARTITION BY kb.barcode,
+    kb.name
+ORDER BY
+    LENGTH(kb.description) DESC) row_number
+from
+    kv_barcode as kb
+join kv_barcode_with_name_most_use as kbw
+		on
+    kb.barcode = kbw.barcode
+    and kb.name = kbw.name
+)
+
+select
+    base64((CONCAT(CAST(L.barcode AS STRING), "-", CAST(L.industry AS STRING)))) as _id
+  ,
+    L.industry AS industry_origin
+  ,
+    L.name as content
+    -- Tên sản phẩm
+  ,
+    L.barcode AS barcode
+  ,
+    CASE
+        WHEN L.description IS NULL THEN ''
+        ELSE L.description
+    END AS description
+  ,
+    L.industry_name
+    -- Tên ngành hàng
+  ,
+    CASE
+        WHEN L.industry = 1
+        OR L.industry = 2 THEN 101
+        WHEN L.industry = 9
+        OR L.industry = 11
+        OR L.industry = 15 THEN 100
+        WHEN L.industry = 12
+        OR L.industry = 13 THEN 102
+        ELSE L.industry
+    END AS industry_new
+,
+    CASE
+        WHEN L.images IS NULL THEN ''
+        ELSE L.images
+    END AS img
+,
+    CASE
+        WHEN L.barcode = ""
+        OR L.barcode IS NULL THEN 0
+        ELSE 1
+    END AS with_barcode
+,
+    L.timestamp
+from
+    kv_barcode_final as L
+where
+    L.row_number = 1;

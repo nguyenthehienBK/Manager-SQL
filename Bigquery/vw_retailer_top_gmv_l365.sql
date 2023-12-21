20319bc291b53c50a39b57c9c487aafa7a0c8268
hien.nt8 <hien.nt8@kiotviet.com.vn>
2023-12-21 17:52:39 +0700

    sync

--- Bigquery/vw_retailer_top_gmv_l365.sql
+++ Bigquery/vw_retailer_top_gmv_l365.sql
@@ -1,7 +1,12 @@
-SELECT  
-  merchant_id
-  ,merchant_key
-  ,gmv_l365
-FROM `kiotvietplus.merchant_profile_warehouse.merchant_transaction` 
-ORDER BY gmv_l365 DESC
-LIMIT 100
+WITH SORT_GMV AS (
+  SELECT  
+    -- merchant_id
+    merchant_key
+    ,ROW_NUMBER() OVER (PARTITION BY r.industry_key ORDER BY gmv_l365 DESC) AS rn
+    -- ,gmv_l365
+  FROM `kiotvietplus.merchant_profile_warehouse.merchant_transaction` AS mt 
+  JOIN  `kiotvietplus.kv_datawarehouse.retailer` AS r ON mt.merchant_key = r.retailer_key
+)
+
+SELECT merchant_key FROM SORT_GMV
+WHERE rn < 50
